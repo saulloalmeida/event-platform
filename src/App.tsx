802f8d1c9aff7e38804f99c8477e09f188020cef
Saullo <saullorhamon@gmail.com>
2022-06-21 06:09:55 -0300

    configuracao apoloclient e graphql

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
+import { gql, useQuery } from "@apollo/client";
+import { useEffect } from "react";
+import { client } from "./lib/apollo";
+
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons {
+      id
+      title
+    }
+  }
+`;
+interface Lesson{
+  id: string;
+  title: string;
+}
 export function App() {
+  const {data} = useQuery<{lessons: Lesson[]}>(GET_LESSONS_QUERY);
+  console.log(data)
   return (
-  <div>Hello</div>
-  )
+    <ul>
+      {data?.lessons.map(lesson => (
+        <li key={lesson.id}>{lesson.title}</li>
+      ))}
+    </ul>
+  );
 }
