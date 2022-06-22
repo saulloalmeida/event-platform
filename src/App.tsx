373c8c59dcb001d289fdc64c9e7d8ffab13696ce
Saullo <saullorhamon@gmail.com>
2022-06-21 21:17:34 -0300

    add header, sidebar e integração com graphcms graphql

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,13 @@
-import { gql, useQuery } from "@apollo/client";
-import { useEffect } from "react";
-import { client } from "./lib/apollo";
+import { Header } from "./components/Header";
+import { Lesson } from "./components/Lesson";
+import { Sidebar } from "./components/Sidebar";
+import { Video } from "./components/Video";
+import { Event } from "./pages/Event";
 
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`;
-interface Lesson{
-  id: string;
-  title: string;
-}
 export function App() {
-  const {data} = useQuery<{lessons: Lesson[]}>(GET_LESSONS_QUERY);
-  console.log(data)
   return (
-    <ul>
-      {data?.lessons.map(lesson => (
-        <li key={lesson.id}>{lesson.title}</li>
-      ))}
-    </ul>
+    <div>
+      <Event />
+    </div>
   );
 }
